fa9cf8c0e6c7933876176007ffc466d33d7caef1
Hwan <106227834+Hwan0405@users.noreply.github.com>
2023-04-25 11:14:54 +0800

    upload new file

---
+++ list.py
@@ -0,0 +1,11 @@
+# list pracitce
+a = ['hwan', 'kate', 'Allan']
+print(a)
+print(a[0])
+print(a[1])
+print(len(a))
+a.append('Declan')
+print(a)
+print(len(a))
+print('hwan' in a)
+print('Andy' in a)
