d09298d6bc6ed949779b6a5471c4f591e1e6d5ab
Hwan <106227834+Hwan0405@users.noreply.github.com>
2023-04-25 11:36:47 +0800

    upload new file

---
+++ for_practice.py
@@ -0,0 +1,10 @@
+students = ['Allen', 'Tom', 'Mayday', 'JJ', 'Jolin', 'Jay', 'jAM']
+for student in students:
+    print('Hi' + ',' + student + '!')
+
+print('----------------------------------------------------------------------------------')
+hwan = 'Hwan'
+for name in hwan:
+    print(name)
+
+print('length of hwan:', len(hwan))
